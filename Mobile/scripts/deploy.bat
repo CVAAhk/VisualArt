6b356bdfbf12a0852e97dd6927b2fb4614509eb9
Shaun Marsh <shaunzillam@yahoo.com>
2017-07-30 20:43:14 -0600

    updated deploy script

--- Mobile/scripts/deploy.bat
+++ Mobile/scripts/deploy.bat
@@ -1,4 +1,4 @@
-PUSHD c:\Qt\5.5\msvc2013_64\bin
+PUSHD c:\Qt\5.8\msvc2015_64\bin
 START /W windeployqt.exe --qmldir c:\repo\omeka-everywhere\Mobile\OmekaMobile c:\repo\omeka-everywhere\Mobile\Builds\Desktop\debug
 POPD
 ROBOCOPY c:\repo\omeka-everywhere\Mobile\Builds\Desktop\debug  files\application\debug /mir
